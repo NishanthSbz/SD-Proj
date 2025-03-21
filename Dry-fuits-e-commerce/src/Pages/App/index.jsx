43dc430fc2dc233b2aa73998129f902264e45951
NishanthSbz <nishanthsbz05@gmail.com>
2025-03-21 15:38:30 +0530

    App contents completed along with routes

--- Dry-fuits-e-commerce/src/Pages/App/index.jsx
+++ Dry-fuits-e-commerce/src/Pages/App/index.jsx
@@ -1,46 +1,130 @@
-import { useRoutes, BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "../../Context/AuthContext"; // Import AuthProvider
+import PropTypes from "prop-types";
+import { useRoutes, BrowserRouter, Navigate } from "react-router-dom";
+import { AppProvider } from "@/Context";
+
+import { AuthProvider } from "@/Context/auth"; // AuthContext is the context that will be used to store the user's data
+import Navbar from "@/Components/Navbar";
+import Footer from "@/Components/Footer";
+import CheckoutSideMenu from "@/Components/CheckoutSideMenu";
+import "./App.css";
+
 import Home from "../Home";
-import MyAccount from "../MyAccount";
 import MyOrder from "../MyOrder";
 import MyOrders from "../MyOrders";
 import NotFound from "../NotFound";
+import MyAccount from "../MyAccount";
 import SignIn from "../SignIn";
-import Navbar from "../../Components/Navbar/Navbar";
-import CheckoutSideMenu from "../../Components/CheckoutSideMenu/CheckoutSideMenu";
-import PrivateRoute from "../../routes/PrivateRoute"; // Import PrivateRoute
+// import Logout from "../Logout";
+
+// Implementing the Private and Public Routes:
+import { useAuth } from "@/Context/auth"; // make sure you have a useAuth hook in your auth context
+
+const PrivateRoute = ({ children }) => {
+  const { user } = useAuth();
+  return user ? children : <Navigate to="/sign-in" />;
+};
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node,
+};
+
+const PublicRoute = ({ children }) => {
+  const { user } = useAuth();
+  return user ? <Navigate to="/" /> : children;
+};
+PublicRoute.propTypes = {
+  children: PropTypes.node,
+};
 
 const AppRoutes = () => {
   let routes = useRoutes([
     { path: "/", element: <Home /> },
-    { path: "/clothes", element: <Home /> },
-    { path: "/electronics", element: <Home /> },
-    { path: "/furnitures", element: <Home /> },
-    { path: "/toys", element: <Home /> },
-    { path: "/others", element: <Home /> },
-    { path: "/sign-in", element: <SignIn /> },
+    { path: "/smartphones", element: <Home /> },
+    { path: "/laptops", element: <Home /> },
+    { path: "/fragrances", element: <Home /> },
+    { path: "/skincare", element: <Home /> },
+    { path: "/groceries", element: <Home /> },
+    { path: "/home-decoration", element: <Home /> },
+    // Private Routes
+    {
+      path: "/my-order",
+      element: (
+        <PrivateRoute>
+          <MyOrder />
+        </PrivateRoute>
+      ),
+    },
+    {
+      path: "/my-orders",
+      element: (
+        <PrivateRoute>
+          <MyOrders />
+        </PrivateRoute>
+      ),
+    },
+    {
+      path: "/my-orders/last",
+      element: (
+        <PrivateRoute>
+          <MyOrder />
+        </PrivateRoute>
+      ),
+    },
+    {
+      path: "/my-orders/:id",
+      element: (
+        <PrivateRoute>
+          <MyOrder />
+        </PrivateRoute>
+      ),
+    },
+    {
+      path: "/my-account",
+      element: (
+        <PrivateRoute>
+          <MyAccount />
+        </PrivateRoute>
+      ),
+    },
+    // {
+    //   path: "/logout",
+    //   element: (
+    //     <PrivateRoute>
+    //       <Logout />
+    //     </PrivateRoute>
+    //   ),
+    // },
+    // Public Routes
+    {
+      path: "/sign-in",
+      element: (
+        <PublicRoute>
+          <SignIn />
+        </PublicRoute>
+      ),
+    },
+    // Not Found
     { path: "*", element: <NotFound /> },
-
-    // Protected Routes
-    { path: "/my-account", element: <PrivateRoute><MyAccount /></PrivateRoute> },
-    { path: "/my-order", element: <PrivateRoute><MyOrder /></PrivateRoute> },
-    { path: "/my-orders", element: <PrivateRoute><MyOrders /></PrivateRoute> },
-    { path: "/my-orders/last", element: <PrivateRoute><MyOrder /></PrivateRoute> },
-    { path: "/my-orders/:id", element: <PrivateRoute><MyOrder /></PrivateRoute> },
   ]);
   return routes;
 };
 
 const App = () => {
   return (
-    <AuthProvider>
+    <AppProvider>
       <BrowserRouter>
-        <AppRoutes />
-        <Navbar />
-        <CheckoutSideMenu />
+        <ma className="flex min-h-screen flex-col">
+          <AuthProvider>
+            <AppRoutes />
+            <Navbar />
+            <CheckoutSideMenu />
+            {/* Content above Footer should grow to push Footer to the bottom */}
+            <div className="flex-grow"></div>
+          </AuthProvider>
+          <Footer />
+        </ma>
       </BrowserRouter>
-    </AuthProvider>
+    </AppProvider>
   );
 };
-
 export default App;
